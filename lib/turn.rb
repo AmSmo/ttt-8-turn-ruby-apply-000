11c244ca3c4da0b3886a7fdf2aee2df21f331e78
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-03 18:51:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,11 +21,7 @@ def valid_move?(board, pos)
 end
 
 def position_taken?(board,move)
-  if board[move] == " " || board[move] == "" || board[move] == NIL
-    return FALSE
-  else
-    return TRUE
-  end
+  return !(board[move] == " " || board[move] == "" || board[move] == NIL)
 end
 
 def move(board, pos, char = "X")
